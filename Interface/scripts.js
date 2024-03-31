fb389d1e51becb35d2c2305fc2ea7c0d9277c2c6
Rogério Alves <rogerio.alves651@gmail.com>
2024-03-31 19:29:54 -0300

    Pasta adicionada.

---
+++ Interface/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("botaoSaudar").addEventListener("click", function() {
+    var mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.innerHTML = "Olá, bem-vindo à interface de usuário com JavaScript!";
+});
