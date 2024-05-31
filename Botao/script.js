1d929ef0a2764e2f47a3b26bfbd9c60d2459e994
Desktop <rogerio.alves651@gmail.com>
2024-05-31 15:06:17 -0300

    Arquivo alterado.

--- Botao/script.js
+++ Botao/script.js
@@ -3,5 +3,6 @@ const botao = document.getElementById('meuBotao');
 botao.addEventListener('click', function() {
     botao.textContent = 'Você clicou em mim!';
     botao.style.backgroundColor = 'red';
+    botao.style.color = 'white';
     botao.style.fontSize = '20px';
 });
