cbbb2f0ec0282cd739be7943c3b45e54b88dcbb3
Desktop <rogerio.alves651@gmail.com>
2024-05-31 14:59:49 -0300

    Arquivo criado.

---
+++ Botao/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById('meuBotao');
+
+botao.addEventListener('click', function() {
+    botao.textContent = 'Você clicou em mim!';
+    botao.style.backgroundColor = 'red';
+    botao.style.fontSize = '20px';
+});
