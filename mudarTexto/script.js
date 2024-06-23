13966db8f90da2bbfec7262eea7efa3d043a598f
Desktop <rogerio.alves651@gmail.com>
2024-06-23 19:28:33 -0300

    Arquivo criado.

---
+++ mudarTexto/script.js
@@ -0,0 +1,10 @@
+const botao = document.getElementById('meuBotao');
+const titulo = document.querySelector('h1');
+const paragrafo = document.querySelector('p');
+
+botao.addEventListener('click', function() {
+    titulo.textContent = "Novo Título";
+    paragrafo.textContent = "Novo parágrafo com conteúdo dinâmico!";
+    botao.textContent = "Botão clicado";
+    botao.style.backgroundColor = "#F4A460";
+});
