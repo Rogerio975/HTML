0d76268de3e199a5d2933bc32429a11486cc744f
Rogério Alves <rogerio.alves651@gmail.com>
2024-07-03 13:24:24 -0300

    Arquivo modificado. Arquivo criado.

---
+++ Formulario/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('example-form');
+
+    form.addEventListener('submit', function(event) {
+        // Previne o envio do formulário para verificar os dados
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const senha = document.getElementById('senha').value;
+        const email = document.getElementById('email').value;
+
+        // Verificação básica dos campos
+        if (nome === '' || senha === '' || email === '') {
+            alert('Todos os campos são obrigatórios!');
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            alert('Por favor, insira um email válido!');
+            return;
+        }
+
+        // Se todos os campos forem válidos, envie o formulário
+        alert('Formulário enviado com sucesso!');
+        form.submit();
+    });
+
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+});
