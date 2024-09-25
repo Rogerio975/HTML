2336ef59983129a6fd2092003319258d1ccda077
Rogério Alves <rogerio.alves651@gmail.com>
2024-09-25 17:03:05 -0300

    Arquivo alterado.

--- TrocarCores/TrocarCoresComIntervalo/script.js
+++ TrocarCores/TrocarCoresComIntervalo/script.js
@@ -1,10 +1,21 @@
-const button = document.getElementById('change-color');
 const body = document.body;
+let intervalo;
 
 function mudarCor() {
   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
   body.style.backgroundColor = randomColor;
-};
+}
 
-// Chama a função mudarCor a cada 4 segundos
-setInterval(mudarCor, 4000);
+function iniciarTrocaCor() {
+  intervalo = setInterval(mudarCor, 4000);
+}
+
+function pausarTrocaCor() {
+  clearInterval(intervalo);
+}
+
+const botaoIniciar = document.getElementById('start');
+const botaoPausar = document.getElementById('pause');
+
+botaoIniciar.addEventListener('click', iniciarTrocaCor);
+botaoPausar.addEventListener('click', pausarTrocaCor);
