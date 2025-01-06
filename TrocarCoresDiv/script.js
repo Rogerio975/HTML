aa95637400b22cba935b1cdcc48c8710a95b4cb4
Rogério <rogerio.alves651@gmail.com>
2025-01-05 21:19:23 -0300

    Projeto criado.

---
+++ TrocarCoresDiv/script.js
@@ -0,0 +1,11 @@
+document.getElementById('colorBox').addEventListener('click', function() {
+    const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', '#4169E1', 'rgb(177, 108, 177)'];
+    const currentColor = this.style.backgroundColor;
+    let newColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    while (newColor === currentColor) {
+        newColor = colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    this.style.backgroundColor = newColor;
+});
