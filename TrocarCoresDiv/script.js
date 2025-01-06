6dcf83f837151c8f9ae47e718b22a69f4e5eae61
Rogério <rogerio.alves651@gmail.com>
2025-01-05 21:27:37 -0300

    console.log adicionada na linha 3.

--- TrocarCoresDiv/script.js
+++ TrocarCoresDiv/script.js
@@ -1,5 +1,7 @@
 document.getElementById('colorBox').addEventListener('click', function() {
     const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', '#4169E1', 'rgb(177, 108, 177)'];
+    console.log('Números de cores disponíveis: ' + colors.length);
+    
     const currentColor = this.style.backgroundColor;
     let newColor = colors[Math.floor(Math.random() * colors.length)];
     
