81582b60e9b063ef90d8c3914c88e1e05dcd77bd
Rogério <rogerio.alves651@gmail.com>
2025-01-11 23:43:56 -0300

    Adiciona função para criar novas divs ao clicar na div inicial

--- Container/ContainerSimples/script.js
+++ Container/ContainerSimples/script.js
@@ -1,10 +1,13 @@
-const container = document.querySelector('.container');
 let divCounter = 2; // Inicia o contador a partir da 2ª div
 
-container.addEventListener('click', () => {
+function createDiv() {
     const newDiv = document.createElement('div');
     newDiv.className = 'container';
     newDiv.innerHTML = `<h1>Container ${divCounter}</h1><p>Este é o container ${divCounter}.</p>`;
+    newDiv.addEventListener('click', createDiv);
     document.body.appendChild(newDiv);
     divCounter++; // Incrementa o contador
-});
+}
+
+const initialContainer = document.querySelector('.container');
+initialContainer.addEventListener('click', createDiv);
