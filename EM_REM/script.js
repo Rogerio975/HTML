ceee35b0bfbc0f2e4d282e0c23f43292eaad3436
Rogério Alves <rogerio.alves651@gmail.com>
2025-07-28 15:40:12 -0300

    refatora estilo e lógica de mudança de cor do botão

--- EM_REM/script.js
+++ EM_REM/script.js
@@ -1,3 +1,12 @@
 document.getElementById('meuBotao').addEventListener('click', function() {
     document.getElementById('minhaDiv').style.backgroundColor = '#fff9c4'; // amarelo claro
 });
+let isYellow = true;
+document.getElementById('meuBotao').addEventListener('click', function() {
+    if (isYellow) {
+        document.getElementById('minhaDiv').style.backgroundColor = '';
+    } else {
+        document.getElementById('minhaDiv').style.backgroundColor = '#fff9c4';
+    }
+    isYellow = !isYellow;
+});
